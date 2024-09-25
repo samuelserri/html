3b224c9fa1c02dca32dcba87ad952a884f5d4c77
Samuel Serri <samuelcerri@gmail.com>
2024-09-24 21:25:33 -0400

    pagina de login

---
+++ login/banco.py
@@ -0,0 +1,14 @@
+# create database meu_banco;
+# use meu_banco;
+# create table Usuario(
+# 	id int auto_increment primary key,
+# 	nome varchar(100),
+#     email varchar(100),
+#     senha varchar(255)
+# );
+# insert into Usuario(nome, email, senha)
+# values('jonatan', 'samuelcerri@gmail.com', sha2('123456', 256)),
+# ('maria', 'maria@gmail.com', sha2('654321', 256));
+
+
+# select * from Usuario;
